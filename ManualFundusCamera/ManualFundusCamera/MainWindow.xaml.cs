3cbdad1c737d7f3321a6664bdaa4b3bdc1955325
Jang Sunghwan <qkkq3883@gmail.com>
2023-12-27 21:13:26 +0900

    카메라 2대 영상 출력

--- ManualFundusCamera/ManualFundusCamera/MainWindow.xaml.cs
+++ ManualFundusCamera/ManualFundusCamera/MainWindow.xaml.cs
@@ -24,6 +24,15 @@ namespace ManualFundusCamera
     {
         private Thread cameraThread;
         private bool shallExitThread;
+        private int retinaAreaX;
+        private int retinaAreaY;
+        private int retinaAreaWidth;
+        private int retinaAreaHeight;
+        private int corneaAreaX;
+        private int corneaAreaY;
+        private int corneaAreaWidth;
+        private int corneaAreaHeight;
+        private IntPtr windowHandle;
 
         public MainWindow()
         {
@@ -32,52 +41,47 @@ namespace ManualFundusCamera
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine(retinaCameraBorder.Margin);
-            Console.WriteLine(retinaCameraBorder.Width);
+            retinaAreaX = (int)retinaCameraBorder.Margin.Left;
+            retinaAreaY = (int)retinaCameraBorder.Margin.Top;
+            retinaAreaWidth = (int)retinaCameraBorder.Width;
+            retinaAreaHeight = (int)retinaCameraBorder.Height;
+
+            corneaAreaX = (int)corneaCameraBorder.Margin.Left;
+            corneaAreaY = (int)corneaCameraBorder.Margin.Top;
+            corneaAreaWidth = (int)corneaCameraBorder.Width;
+            corneaAreaHeight = (int)corneaCameraBorder.Height;
+
+            windowHandle = new WindowInteropHelper(this).Handle;
 
             cameraThread = new Thread(doCameraThread);
             cameraThread.Start();
         }
 
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            
-        }
-
         private void doCameraThread()
         {
-            int retinaAreaX = 0;
-            int retinaAreaY = 0;
-            int retinaAreaWidth = 0;
-            int retinaAreaHeight = 0;
-
-            int corneaAreaX = 0;
-            int corneaAreaY = 0;
-            int corneaAreaWidth = 0;
-            int corneaAreaHeight = 0;
-
-            Dispatcher.Invoke(() =>
-            {
-                retinaAreaX = (int)retinaCameraBorder.Margin.Left;
-                retinaAreaY = (int)retinaCameraBorder.Margin.Top;
-                retinaAreaWidth = (int)retinaCameraBorder.Width;
-                retinaAreaHeight = (int)retinaCameraBorder.Height;
-
-                corneaAreaX = (int)corneaCameraBorder.Margin.Left;
-                corneaAreaY = (int)corneaCameraBorder.Margin.Top;
-                corneaAreaWidth = (int)corneaCameraBorder.Width;
-                corneaAreaHeight = (int)corneaCameraBorder.Height;
-            });
-
             shallExitThread = false;
 
-            Statics.initializeCameras();
-            Statics.initializeWindow(new WindowInteropHelper(this).Handle);
+            Statics.ErrorInitializeCameras errorInitializeCameras = Statics.initializeCameras();
+            if (errorInitializeCameras != Statics.ErrorInitializeCameras.None)
+            {
+                Console.WriteLine(errorInitializeCameras);
+            }
+
+            Statics.initializeWindow(windowHandle);
 
             while (!shallExitThread)
             {
-                Statics.showCameraFrame(Statics.Part.Retina, retinaAreaX, retinaAreaY, retinaAreaWidth, retinaAreaHeight);
-                Statics.showCameraFrame(Statics.Part.Cornea, corneaAreaX, corneaAreaY, corneaAreaWidth, corneaAreaHeight);
+                Statics.ErrorShowCameraFrame errorShowCameraFrame = Statics.showCameraFrame(Statics.Part.Retina, retinaAreaX, retinaAreaY, retinaAreaWidth, retinaAreaHeight);
+                if (errorShowCameraFrame != Statics.ErrorShowCameraFrame.None)
+                {
+                    Console.WriteLine(errorShowCameraFrame);
+                }
+
+                errorShowCameraFrame = Statics.showCameraFrame(Statics.Part.Cornea, corneaAreaX, corneaAreaY, corneaAreaWidth, corneaAreaHeight);
+                if (errorShowCameraFrame != Statics.ErrorShowCameraFrame.None)
+                {
+                    Console.WriteLine(errorShowCameraFrame);
+                }
             }
 
             Statics.closeCameras();
@@ -86,8 +90,12 @@ namespace ManualFundusCamera
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
         {
-            shallExitThread = true;
             Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            shallExitThread = true;
         }
     }
 }
